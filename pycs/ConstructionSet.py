416272101664c402e5a11770569ebc6c212df8fe
j <joelsouthgate@googlemail.com>
2021-10-17 09:34:59 +0100

    move image selector away

--- pycs/ConstructionSet.py
+++ pycs/ConstructionSet.py
@@ -1,78 +1,13 @@
 import PySimpleGUI as sg
-from pycs.CanvasCamera import CanvasCamera
+from pycs.CanvasImageViewer import CanvasImageViewer
 from pycs.WorldModel import WorldModel
+from pycs.ImageCollection import ImageCollection
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
 
-class ImageSelector:
-    """ 
-    Class responsible for selection of objects
-    """
-    def __init__(self):
-
-        self.focused_image = 2
-        self.tab_counter = 0
-        self.origin_x = None
-        self.origin_y = None
-        self.picked_up = False
-
-    def click(self, x, y):
-        self.origin_x = x
-        self.origin_y = y
-    
-    def release(self):
-        if self.picked_up:
-            print("moving!")
-            self.picked_up = False
-            self.picker_offset_x = 0
-            self.picker_offset_y = 0
-
-    def select_next_from_list(self, l, mod=1):
-        if l:
-            self.tab_counter += ( 1 * mod )
-            if self.tab_counter > len(l) - 1:
-                self.tab_counter = 0
-            if self.tab_counter < 0:
-                self.tab_counter = len(l) - 1
-            choice = l[self.tab_counter]
-            self.select_image(choice)
-            return self.focused_image
-        return None      
-
-    def select_next_image(self, near, x, y):
-        selected = self.select_next_from_list(near)
-        if selected:
-            print("selected a new image!", selected)
-            return selected
-        else:
-            print("nothing selected!")
-        return None
-
-    def select_prev_image(self, near, x, y):
-        selected = self.select_next_from_list(near, -1)
-        if selected:
-            print("selected a new image!", selected)
-            return selected
-        else:
-            print("nothing selected!")
-        return None
-
-#    def find_near(self, canvas, x, y, halo=250):
-#        found = canvas.find_overlapping(x - halo, y - halo, x + halo, y + halo)
-#        print("found", found, "at", x, y)
-#        return found
-
-    def select_image(self, n):
-        self.focused_image = n
-        self.picked_up = True
-
-    def get_selected_image_coords(self, canvas):
-        x, y = canvas.coords(self.focused_image)
-        return x, y    
-
 class ConstructionSetApp:
 
-    def __init__(self, wm):
+    def __init__(self, wm: WorldModel, image_viewer: CanvasImageViewer, ic: ImageCollection):
 
         self.WIDTH, self.HEIGHT = sg.Window.get_screen_size()
         self.CANVAS_PROP = 0.95
