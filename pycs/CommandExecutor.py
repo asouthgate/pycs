0d0a2ad7a3c5f06fe2f4612b0a5973680c180abd
j <joelsouthgate@googlemail.com>
2021-11-01 08:58:24 +0000

    basic logic for command queue

---
+++ pycs/CommandExecutor.py
@@ -0,0 +1,30 @@
+from pycs.interfaces.CommandABC import CommandABC
+
+class CommandExecutor:
+    """ Class used to manage execution of commands. """
+
+    def __init__(self):
+        self.canvas = canvas
+        # Inverse command history to record operations needed to undo
+        self.command_history = []
+        # Every undo operation pops from command_history and adds to undo_history for redo
+        self.undo_history = []
+
+    def execute(self, command: CommandABC):
+        """ Execute a command """
+        command.execute()
+        self.command_history.append(command)
+    
+    def undo(self):
+        """ Undo a command """
+        command = self.command_history.pop()
+        command.undo()
+        self.undo_history.append(command)
+
+    def redo(self):
+        """ Redo a command """
+        command = self.undo_history.pop()
+        command.execute()
+        self.command_history.append(commmand)
+
+
---
+++ pycs/interfaces/CommandABC.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    """ Interface for executed UI commands. 
+    
+    Command objects should be created with all the
+    data they need to execute their functionality. 
+
+    """
+
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+    @abstractmethod
+    def undo(self): -> None:
+        pass
